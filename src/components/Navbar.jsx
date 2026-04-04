890b282aa26a583bc1b6cd4059753d3925bcced5
dhanu <dhanushcg100@gmail.com>
2026-04-04 12:21:43 +0530

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,39 @@
 import "./Navbar.css";
+import {
+  FaShoppingCart,
+  FaBoxOpen,
+  FaSignInAlt,
+  FaUserPlus,
+} from "react-icons/fa";
+
 const Navbar = () => {
   return (
     <nav className="nav">
       <a href="/" className="site-title">
+        <FaShoppingCart className="logo-icon" />
         GenZStore
       </a>
 
       <ul>
         <li>
-          <a href="/login">Login</a>
+          <a href="/products">
+            <FaBoxOpen className="icon" />
+            Products
+          </a>
         </li>
+
         <li>
-          <a href="/signup">SignUp</a>
+          <a href="/login">
+            <FaSignInAlt className="icon" />
+            Login
+          </a>
+        </li>
+
+        <li>
+          <a href="/signup">
+            <FaUserPlus className="icon" />
+            SignUp
+          </a>
         </li>
       </ul>
     </nav>
