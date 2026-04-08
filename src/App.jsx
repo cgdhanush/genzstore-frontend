b9ef04a9c20a114d6415d138759c4b28e1b749a2
dhanu <dhanushcg100@gmail.com>
2026-04-08 10:53:46 +0530

    fixed some issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Login from "./pages/auth/Login";
 import SignUp from "./pages/auth/SignUp";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import Cart from "./pages/Cart";
+
 import "./App.css"
 
 function App() {
@@ -13,6 +15,9 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
+
+        <Route path="/cart" element={<Cart />} />
+
 
         {/* Catch-all route for unmatched URLs */}
         <Route path="*" element={<NotFound />} />
