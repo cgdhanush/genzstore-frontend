bcbd0b060051861b2f0aff39d23c5b4c65026792
dhanu <dhanushcg100@gmail.com>
2026-04-11 11:58:21 +0530

    added product file for tesing

---
+++ src/data/products.js
@@ -0,0 +1,54 @@
+import Image from "../assets/shoping.jpg";
+
+const products = [
+  {
+    id: 1,
+    name: "Oversized Hoodie",
+    price: 1499,
+    image: Image,
+  },
+  {
+    id: 2,
+    name: "Sneakers",
+    price: 2999,
+    image: Image,
+  },
+  {
+    id: 3,
+    name: "Cap",
+    price: 499,
+    image: Image,
+  },
+  {
+    id: 4,
+    name: "Cap",
+    price: 499,
+    image: Image,
+  },
+  {
+    id: 5,
+    name: "Oversized Hoodie",
+    price: 1499,
+    image: Image,
+  },
+  {
+    id: 6,
+    name: "Sneakers",
+    price: 2999,
+    image: Image,
+  },
+  {
+    id: 7,
+    name: "Cap",
+    price: 499,
+    image: Image,
+  },
+  {
+    id: 8,
+    name: "Cap",
+    price: 499,
+    image: Image,
+  },
+];
+
+export default products;
