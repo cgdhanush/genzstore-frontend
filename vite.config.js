59baa93e2d5f8f5724f85635a440c4ba51e8ffe3
dhanu <dhanushcg100@gmail.com>
2026-04-16 14:17:14 +0530

    config the server

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
+  plugins: [react()],
+   server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8080',
+        changeOrigin: true,
+      },
+    },
+    host: '127.0.0.1',
     port: 3000,
     strictPort: true,
   },
