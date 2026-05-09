34b363390711e432deb38838658940a08b5590cc
dhanu <dhanushcg100@gmail.com>
2026-05-09 19:40:18 +0530

    added api fetching for products

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,26 +1,51 @@
-import {
-  Card,
-  Image,
-  Text,
-  Group,
-  SimpleGrid,
-  Badge,
-} from "@mantine/core";
+import { Card, Image, Text, Group, SimpleGrid, Badge } from "@mantine/core";
 
-import { productsData } from "../data.ts";
 import type { Product } from "../types/product.ts";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import CartButton from "./button/CartButton.tsx";
+import apiClient from "../services/api-client.ts";
+
+// import { productsData } from "../data.ts";
 
 export default function ProductGrid() {
-  const [products, ] = useState<Product[]>(productsData);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const baseURL: string = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setLoading(true);
+
+        const data = await apiClient.get<Product[]>("/products");
+        setProducts(data);
+      } catch (err) {
+        setError("Failed to fetch products");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
 
   return (
     <SimpleGrid cols={{ base: 1, sm: 2, md: 3 }} spacing="lg">
       {products.map((product) => (
         <Card key={product.id} shadow="sm" padding="lg" radius="md" withBorder>
           <Card.Section>
-            <Image src={product.imageId} height={180} alt={product.name} />
+            <Image
+              src={`${baseURL}/products/${product.imageId}/image`}
+              height={180}
+              alt={product.name}
+              fit="cover"
+            />
           </Card.Section>
 
           <Group justify="space-between" mt="md" mb="xs">
@@ -32,7 +57,7 @@ export default function ProductGrid() {
             ₹{product.price}
           </Text>
 
-          <CartButton product={product}/>
+          <CartButton product={product} />
         </Card>
       ))}
     </SimpleGrid>
