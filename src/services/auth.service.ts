ab880ed260541ff26fc870a98071260ad63a84d0
dhanu <dhanushcg100@gmail.com>
2026-05-09 23:14:35 +0530

    updaed the auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,30 +1,6 @@
 import apiClient from "./api-client";
 
 //
-// LOGIN
-//
-export type LoginData = {
-  email: string;
-  password: string;
-};
-
-export type LoginResponse = {
-  access_token: string;
-  user: {
-    id: number;
-    name: string;
-    email: string;
-  };
-};
-
-export async function login(data: LoginData) {
-  return apiClient.post<LoginResponse>(
-    "/auth/login",
-    data
-  );
-}
-
-//
 // REGISTER
 //
 export type RegisterData = {
@@ -43,8 +19,37 @@ export type RegisterResponse = {
 };
 
 export async function register(data: RegisterData) {
-  return apiClient.post<RegisterResponse>(
-    "/auth/signup",
-    data
-  );
+  return apiClient.post<RegisterResponse>("/auth/signup", data);
+}
+
+//
+// LOGIN
+//
+export type LoginData = {
+  email: string;
+  password: string;
+};
+
+export type LoginResponse = {
+  access_token: string;
+  user: {
+    id: number;
+    name: string;
+    email: string;
+  };
+};
+
+export async function login(data: LoginData) {
+  return apiClient.post<LoginResponse>("/auth/login", data);
+}
+
+//
+// LOGOUT
+//
+export type LogoutResponse = {
+  message: string;
+};
+
+export async function logout() {
+  return apiClient.post<LogoutResponse>("/auth/logout");
 }
