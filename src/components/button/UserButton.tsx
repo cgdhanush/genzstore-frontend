f4e061f8a50ecb77890e8d4bc46225d4194b3fab
dhanu <dhanushcg100@gmail.com>
2026-05-09 23:16:45 +0530

    updated the user button

--- src/components/button/UserButton.tsx
+++ src/components/button/UserButton.tsx
@@ -1,23 +1,56 @@
-import { ActionIcon } from "@mantine/core";
-import { IconUser } from "@tabler/icons-react";
+import { ActionIcon, Menu } from "@mantine/core";
+import { IconUser, IconLogout } from "@tabler/icons-react";
 import { useNavigate } from "react-router-dom";
+import { logout } from "../../services/auth.service";
 
 export default function UserButton() {
   const navigate = useNavigate();
 
-  const handleClick = () => {
-    const token = localStorage.getItem("access_token");
+  const token = localStorage.getItem("access_token");
+  const isLoggedIn = !!token;
 
-    if (token) {
-      navigate("/user");
-    } else {
-      navigate("/auth");
+  const handleProfile = () => {
+    navigate("/user");
+  };
+
+  const handleAuth = () => {
+    navigate("/auth");
+  };
+  const handleLogout = async () => {
+    try {
+      await logout();
+      localStorage.removeItem("access_token");
+      navigate("/home");
+    } catch (err) {
+      console.error("Logout failed", err);
     }
   };
 
   return (
-    <ActionIcon onClick={handleClick}>
-      <IconUser />
-    </ActionIcon>
+    <Menu shadow="md" width={180}>
+      <Menu.Target>
+        <ActionIcon variant="light">
+          <IconUser />
+        </ActionIcon>
+      </Menu.Target>
+
+      <Menu.Dropdown>
+        {isLoggedIn ? (
+          <>
+            <Menu.Item onClick={handleProfile}>Profile</Menu.Item>
+
+            <Menu.Item
+              color="red"
+              leftSection={<IconLogout size={16} />}
+              onClick={handleLogout}
+            >
+              Logout
+            </Menu.Item>
+          </>
+        ) : (
+          <Menu.Item onClick={handleAuth}>Login</Menu.Item>
+        )}
+      </Menu.Dropdown>
+    </Menu>
   );
 }
