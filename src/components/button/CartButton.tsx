ff294f4050cd2ca0f12493aa025ac49959c3ff7f
dhanu <dhanushcg100@gmail.com>
2026-05-10 00:40:25 +0530

    fixed cart button

--- src/components/button/CartButton.tsx
+++ src/components/button/CartButton.tsx
@@ -1,32 +1,45 @@
 import { Button, Group, ActionIcon, Text } from "@mantine/core";
 import { IconMinus, IconPlus } from "@tabler/icons-react";
+
 import { useCartStore } from "../../store/cartStore";
 import type { Product } from "../../types/product";
 
-export default function CartButton({ product }: {product: Product}) {
+export default function CartButton({ product }: { product: Product }) {
   const items = useCartStore((s) => s.items);
+
   const addToCart = useCartStore((s) => s.addToCart);
   const increaseQty = useCartStore((s) => s.increaseQty);
   const decreaseQty = useCartStore((s) => s.decreaseQty);
 
-  const cartItem = items.find((i) => i.id === product.id);
+  // Match using productId from cart
+  const cartItem = items.find((item) => item.product.id === product.id);
 
-  const qty = cartItem?.qty || 0;
+  const qty = cartItem?.quantity || 0;
 
+  // Not in cart
   if (qty === 0) {
     return (
-      <Button fullWidth onClick={() => addToCart(product)} >
+      <Button
+        fullWidth
+        onClick={() => {
+          addToCart({
+            product: product,
+          });
+        }}
+      >
         Add to Cart
       </Button>
     );
   }
 
-  // If in cart → show +/- controls
+  // In cart
   return (
     <Group justify="space-between" mt="md">
       <ActionIcon
         variant="light"
-        onClick={() => decreaseQty(product.id)}
+        onClick={async () => {
+          await decreaseQty(product);
+        }}
       >
         <IconMinus size={16} />
       </ActionIcon>
@@ -35,7 +48,9 @@ export default function CartButton({ product }: {product: Product}) {
 
       <ActionIcon
         variant="light"
-        onClick={() => increaseQty(product.id)}
+        onClick={async () => {
+          await increaseQty(product);
+        }}
       >
         <IconPlus size={16} />
       </ActionIcon>
